0cffa871404ab07b7f82a2690da93a892c57aab3
adrianbogdann <adrian.bogdann@yahoo.com>
2016-08-16 11:22:34 +0300

    added both 'Add new item' and 'Edit item' in formAction

--- application/modules/default/controllers/ListController.php
+++ application/modules/default/controllers/ListController.php
@@ -38,6 +38,49 @@ class ListController extends Zend_Controller_Action {
         }
     }
 
+    public function formAction() {
+        if ($this->getRequest()->getParam('number') !== null) {
+            $id = $this->getRequest()->getParam('number');
+            $this->editForm = $form = new Application_Forms_Editare();
+
+            $products = new Application_Models_Products();
+            $list = $products->fetchAll(array('productId in (?)' => $id));
+            $row = $list->current();
+            $this->editForm->getElement('id')->setValue($row->productId);
+            $this->editForm->getElement('title')->setValue($row->productTitle);
+            $this->editForm->getElement('desc')->setValue($row->productDesc);
+            $this->editForm->getElement('price')->setValue($row->productPrice);
+            $this->view->form = $this->editForm;
+
+        } else {
+            $this->view->form = $form = new Application_Forms_Editare();
+        }
+        if($this->getRequest()->getParam('id') !== null) {
+            if (!$form->isValid($this->getRequest()->getParams())) {
+                $this->view->mesaj = 'Incorect';
+            } else {
+                $adapter = new Zend_File_Transfer_Adapter_Http();
+                $adapter->setDestination('C:\wamp64\www\etapa2_zend\images');
+                try {
+                    $adapter->receive();
+                } catch (Zend_File_Transfer_Exception $e) {
+                    $e->getMessage();
+                }
+                $photoname = $adapter->getFileName('image', false);
+                $products = new Application_Models_Products();
+                $product = $products->createRow();
+                $product->productId = $this->getRequest()->getParam('id');
+                $product->productImg = $photoname;
+                $product->productTitle = $this->getRequest()->getParam('title');
+                $product->productDesc = $this->getRequest()->getParam('desc');
+                $product->productPrice = $this->getRequest()->getParam('price');
+                $product->save();
+
+                $this->_helper->redirector->gotoUrl('/list');
+            }
+        }
+    }
+
     public function removeAction() {
         $id = $this->getRequest()->getParam('id');
         $products = new Application_Models_Products();
